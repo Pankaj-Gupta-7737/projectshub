8175ebf4ef3cf3ef44aa6bb074209f4efd65de36
unknown <pg007135@gmail.com>
2024-12-29 14:28:10 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpClientModule } from '@angular/common/http';
+import { SafeUrlPipe } from './services/safe-url.pipe';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent,SafeUrlPipe],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../services/movie.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  movies: any[] = [];
+  selectedMovie: { name: string; iframe_url: SafeResourceUrl } | null = null;
+  showModal: boolean = false;
 
-  constructor() {}
+  constructor(private movieService: MovieService, private sanitizer: DomSanitizer) {}
 
+  ngOnInit() {
+    this.movieService.getMovies().subscribe((data) => {
+      this.movies = data.movies;
+      console.log(this.movies);
+    });
+  }
+
+  openModal(movie: any) {
+    this.selectedMovie = {
+      name: movie.name,
+      iframe_url: this.sanitizeUrl(movie.iframe_url),
+    };
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.selectedMovie = null;
+    this.showModal = false;
+  }
+
+  sanitizeUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
